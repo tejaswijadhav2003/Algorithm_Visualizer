3f13d498b0a66d214d5bef8a6a47d16090addab8
Tejaswi Jadhav <tejugeeta110@gmail.com>
2023-07-14 12:03:23 +0530

    add

---
+++ sorting.js
@@ -0,0 +1,21 @@
+function swap(ele1,ele2){
+    let temp=ele1.style.height;
+    ele1.style.height=ele2.style.height;
+    ele2.style.height=temp;
+}
+
+function disablesortingbtn(){
+    document.querySelector(".bubbleSort").disabled=true;
+}
+
+function enablesortingbtn(){
+    document.querySelector(".bubbleSort").disabled=false;
+}
+let array=[];
+createNewArray();
+function createNewArray(){
+    for(let i=0;i<10;i++){
+        array[i]=
+    }
+}
+
